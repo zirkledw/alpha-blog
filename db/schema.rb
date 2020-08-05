e4ef5c50964a62bb8d7d0c836c40318fe3941c04
David Zirkle <zirkledw@hotmail.com>
2020-08-05 19:48:40 -0400

    Add Bcrypt and password digest to users table

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_08_05_194759) do
+ActiveRecord::Schema.define(version: 2020_08_05_210643) do
 
   create_table "articles", force: :cascade do |t|
     t.string "title"
@@ -25,6 +25,7 @@ ActiveRecord::Schema.define(version: 2020_08_05_194759) do
     t.string "email"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.string "password_digest"
   end
 
 end
