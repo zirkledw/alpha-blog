7facd85c70a1167ada26a706aff5a8c64559d8e6
David Zirkle <zirkledw@hotmail.com>
2020-12-14 11:42:08 -0500

    Finish categories edit functionality.

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -5,6 +5,20 @@ class CategoriesController < ApplicationController
     @category = Category.new
   end
 
+  def edit
+    @category = Category.find(params[:id])
+  end
+
+  def update
+    @category = Category.find(params[:id])
+    if @category.update(category_params)
+      flash[:notice] = "Category name updated sucessfully"
+      redirect_to @category
+    else
+      render 'edit'
+    end
+  end
+
   def create
     @category = Category.new(category_params)
     if @category.save
